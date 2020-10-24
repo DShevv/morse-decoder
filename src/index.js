e46c1f0e1cd4477f10a87e8976ea5dee53e79ecd
Dmitry Shevtsov <dshevtsov20@gmail.com>
2020-10-24 16:09:28 +0300

    feat: add morse-decoder func

--- src/index.js
+++ src/index.js
@@ -37,8 +37,31 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+
 function decode(expr) {
-    // write your solution here
+  var message = ''
+  var code = ''
+  var lettesr = expr.match(/.{1,10}/g);
+
+  for (let index = 0; index < lettesr.length; index++) {
+    code = ''
+    if (lettesr[index] != '**********') {
+      for (let i = 0; i < lettesr[index].length; i += 2) {
+        if( lettesr[index][i].toString()+lettesr[index][i+1].toString()  == '10'){
+          code += '.';
+        }
+        if(lettesr[index][i].toString()+lettesr[index][i+1].toString() == '11'){
+          code += '-';
+        }
+      }
+      message += MORSE_TABLE[code]
+     
+    }
+    else{
+      message += ' '
+    }  
+  }
+  return message
 }
 
 module.exports = {
